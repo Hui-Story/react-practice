2aa061cac3adf67119284566c28d3dc708f248aa
Hui-Story <leejaehee0728@gmail.com>
2022-03-11 19:05:36 +0900

    ENG: update sagas.ts
    sagas.ts 주석 추가

--- ts-react-redux-tutorial/src/modules/github/sagas.ts
+++ ts-react-redux-tutorial/src/modules/github/sagas.ts
@@ -6,10 +6,12 @@ function* getUserProfileSaga(
   action: ReturnType<typeof getUserProfileAsync.request>
 ) {
   try {
+    // call : 함수 실행 (함수, 파라미터)
     const userProfile: GithubProfile = yield call(
       getUserProfile,
       action.payload
     );
+    // put : 특정 액션을 dispatch
     yield put(getUserProfileAsync.success(userProfile));
   } catch (e: any) {
     yield put(getUserProfileAsync.failure(e));
